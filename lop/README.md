edbb1d001b7183166952d49a1a1a1156044c8640
Jea Kwon <33548421+jeakwon@users.noreply.github.com>
2025-01-30 21:55:11 +0900

    update

--- lop/README.md
+++
@@ -1,2 +0,0 @@
-The code inside the `lop` folder originates from the following source:  
-[https://github.com/shibhansh/loss-of-plasticity/blob/main/lop/](https://github.com/shibhansh/loss-of-plasticity/blob/main/lop/)
--- nets/torchvision_modified_resnet.py
+++ nets/torchvision_modified_resnet.py
@@ -1,4 +1,4 @@
-# https://github.com/shibhansh/loss-of-plasticity/blob/main/lop/nets/torchvision_modified_resnet.py
+# Copy of https://github.com/shibhansh/loss-of-plasticity/blob/main/lop/nets/torchvision_modified_resnet.py
 from typing import Callable, List, Optional, Type, Union
 
 import torch
