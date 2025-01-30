5f07ccc6e13ba8982e7bae40398b5027c63a52c3
Jea Kwon <33548421+jeakwon@users.noreply.github.com>
2025-01-30 20:21:07 +0900

    update

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='csse',
     packages=find_packages(),
-    version='0.0.2'
+    version='0.0.1',
     install_requires=[
         "mlproj-manager"
-    ]
+    ],
 )
