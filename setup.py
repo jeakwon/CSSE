5e35c965f3ba1bc1fda841c2f7a10584900fd99c
Jea Kwon <33548421+jeakwon@users.noreply.github.com>
2025-01-30 21:16:25 +0900

    del init

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='csse',
     packages=find_packages(),
-    version='0.0.7',
+    version='0.0.8',
     install_requires=[
         "mlproj-manager"
     ],
