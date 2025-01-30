f867c4492a0a0210201340091c4cd1c2ac61f84f
Jea Kwon <33548421+jeakwon@users.noreply.github.com>
2025-01-31 08:50:56 +0900

    update

--- external_codes/mlproj_manager/cifar_data_loader.py
+++ external_codes/mlproj_manager/cifar_data_loader.py
@@ -56,57 +56,7 @@ class CustomDataSet(Dataset):
 
 
 # from mlproj_manager.util.data_preprocessing_and_transformations import normalize, preprocess_labels
-# https://github.com/JFernando4/Research_Project_Manager/blob/master/mlproj_manager/util/data_preprocessing_and_transformations/normalization_and_preprocessing.py
-"""
-Helper functions for normalizing data
-"""
-def normalize(data, norm_type: str, **kwargs):
-    """
-    Normalizes the given data according to the specified type
-    :param data: (np.array or torch.Tensor) array containing the data
-    :param norm_type: (str) type of normalization from the following options:
-                                - None: no normalization
-                                - centered: subtracts the mean and divides by standard deviation
-                                - max: divides the maximum of the data
-                                - minus-one-to-one: divides by the maximum of the data and scales to range(-1,1)
-    :param kwargs: extra optional keyword arguments for the different normalization types. Available options:
-                        - None: no available keyword arguments
-                        - centered: avg = average of the data, stddev = standard deviation of the data
-                        - max: max_val = maximum value of the data
-                        - minus-one-to-one: max_val as above
-                   any necessary statistic used for normalization is computed from the data if not give.
-    :return: normalized numpy or torch array
-    """
-    if norm_type is None:
-        return data
-    elif norm_type == "centered":
-        avg = np.average(np.float32(data)) if "avg" not in kwargs.keys() else kwargs["avg"]
-        stddev = np.std(np.float32(data), ddof=1) if "stddev" not in kwargs.keys() else kwargs["stddev"]
-        return (data - avg) / stddev
-    elif norm_type == "max" or norm_type == "minus-one-to-one":
-        max_val = max(data) if "max_val" not in kwargs.keys() else kwargs["max_val"]
-        if norm_type == "minus-one-to-one":
-            return 2 * (data / max_val) - 1
-        return data / max_val
-    else:
-        raise ValueError("Invalid normalization type: {0}".format(norm_type))
-
-
-def preprocess_labels(labels, preprocessing_type: str):
-    """
-    Preprocesses the given labels according to preprocessing_type
-    :param labels: (numpy or torch array) array containing the labels to be preprocessed
-    :param preprocessing_type: (str) type of preprocessing from the following:
-                                        - None: does nothing
-                                        - one-hot: turns labels into one-hot labels
-    :return: preprocessed labels
-    """
-    if preprocessing_type is None:
-        return labels
-    elif preprocessing_type == "one-hot":
-        return from_integers_to_one_hot(labels)
-    else:
-        raise ValueError("Invalid label preprocessing: {0}".format(preprocessing_type))
+from ccse.external_codes.mlproj_manager.normalization_and_preprocessing import normalize, preprocess_labels
 
 # from mlproj_manager.definitions import CIFAR_DATA_PATH
 # https://github.com/JFernando4/Research_Project_Manager/blob/master/mlproj_manager/definitions.py
