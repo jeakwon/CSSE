fc3c9ea3a99e87bcc474ecb3f9d25a7782396f4d
Jea Kwon <33548421+jeakwon@users.noreply.github.com>
2025-01-30 23:12:58 +0900

    update

--- model/__init__.py
+++ model/__init__.py
@@ -1,3 +1,7 @@
 from csse.lop.nets.torchvision_modified_resnet import build_resnet18
 
-print(4)
+def reconstruct_resnet18(weight_file_path: str):
+    model = build_resnet18(num_classes=100, norm_layer=torch.nn.BatchNorm2d)
+    state_dict = torch.load(weight_file_path, map_location="cpu", weights_only=True)
+    model.load_state_dict(state_dict)
+    return model
