ad7d9083776c1971dd67564b50a3b05a2668c187
Jea Kwon <33548421+jeakwon@users.noreply.github.com>
2025-01-31 21:00:18 +0900

    update

--- utils/experiment.py
+++ utils/experiment.py
@@ -7,6 +7,32 @@ from csse.utils.data import load_cifar100, load_class_order, parse_class_order
 from csse.utils.model import load_lop_resnet18_state_dict
 from csse.utils.evaluate import selected_class_accuracy
 
+class Load_ResNet18_CIFAR100_CIL_Experiment:
+    def __init__(self, algo, seed, sessions=range(21), device='cuda', batch_size=100, num_workers=2):
+        self.algo = algo
+        self.seed = seed
+        self.device = device
+        
+        self.class_order = load_class_order(algo, seed)
+
+        self.train_loader = load_cifar100(train=True, batch_size=batch_size, num_workers=num_workers)
+        self.test_loader = load_cifar100(train=False, batch_size=batch_size, num_workers=num_workers)
+
+        self.backbone = build_resnet18(num_classes=100, norm_layer=torch.nn.BatchNorm2d).to(device)
+        self.sessions = { session: Session(session=session, experiment=self) for session in sessions }
+    
+    def eval_train_acc(self, model, selected_classes):
+        return selected_class_accuracy(model, self.train_loader, selected_classes, self.device)
+    
+    def eval_test_acc(self, model, selected_classes):
+        return selected_class_accuracy(model, self.test_loader, selected_classes, self.device)
+
+    def __getitem__(self, session):
+        return self.sessions[session]
+    
+    def session(self, session):
+        return self.sessions[session]
+
 class Session:
     def __init__(self, session, experiment):
         self.session = session
@@ -46,29 +72,3 @@ class Session:
         for class_name, selected_classes in self.class_info.items():
             accuracies[class_name] = selected_class_accuracy(model, self.test_loader, selected_classes, self.device)
         return accuracies
-
-class Load_ResNet18_CIFAR100_CIL_Experiment:
-    def __init__(self, algo, seed, sessions=range(21), device='cuda', batch_size=100, num_workers=2):
-        self.algo = algo
-        self.seed = seed
-        self.device = device
-        
-        self.class_order = load_class_order(algo, seed)
-
-        self.train_loader = load_cifar100(train=True, batch_size=batch_size, num_workers=num_workers)
-        self.test_loader = load_cifar100(train=False, batch_size=batch_size, num_workers=num_workers)
-
-        self.backbone = build_resnet18(num_classes=100, norm_layer=torch.nn.BatchNorm2d).to(device)
-        self.sessions = { session: Session(session=session, experiment=self) for session in sessions }
-    
-    def eval_train_acc(self, model, selected_classes):
-        return selected_class_accuracy(model, self.train_loader, selected_classes, self.device)
-    
-    def eval_test_acc(self, model, selected_classes):
-        return selected_class_accuracy(model, self.test_loader, selected_classes, self.device)
-
-    def __getitem__(self, session):
-        return self.sessions[session]
-    
-    def session(self, session):
-        return self.sessions[session]
