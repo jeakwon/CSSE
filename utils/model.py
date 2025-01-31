c5ae52efab656008795f1c6c2af6365f51035a71
Jea Kwon <33548421+jeakwon@users.noreply.github.com>
2025-01-31 17:37:36 +0900

    update

--- utils/model.py
+++ utils/model.py
@@ -12,11 +12,15 @@ ALGORITHM = {
     'sp': 'shrink_and_perturb',
 }
 
-def load_lop_resnet18(algo, seed, session):
+def load_lop_resnet18_state_dict(algo, seed, session):
     algorithm = ALGORITHM[algo]
     file_name = f'index-{seed}_epoch-{session*200}.pt'
     url = f"https://huggingface.co/onlytojay/lop-resnet18/resolve/main/{algorithm}/model_parameters/{file_name}"
     state_dict = load_state_dict(url)
+    return state_dict
+
+def load_lop_resnet18(algo, seed, session):
+    state_dict = load_lop_resnet18_state_dict(algo, seed, session)
     model = build_resnet18(num_classes=100, norm_layer=torch.nn.BatchNorm2d)
     model.load_state_dict(state_dict)
     return model
